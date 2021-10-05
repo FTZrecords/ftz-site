215c33bbe9930fc73c19d40e6c3a21d4f8588adc
FTZrecords <ftzrecords@gmail.com>
2021-10-05 10:29:29 +0900

    add All

--- pages/member/[member].js
+++ pages/member/[member].js
@@ -1,8 +1,20 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 import matter from "gray-matter";
-import ReactMarkdown from "react-markdown";
+
+import Markdown from "react-markdown";
+import grayMatter from "gray-matter";
+import remarkGfm from "remark-gfm";
+import unwrapImages from "remark-unwrap-images";
+//HTMLを
+import rehypeRaw from "rehype-raw";
+//タイトルタグにジャンプリンクを
+import remarkSlug from "remark-slug";
+import remarkAutolinkHeadings from "remark-autolink-headings";
+//ショートコード
+import directive from "remark-directive";
 
 import post from "styles/post.module.scss";
 
@@ -27,10 +39,52 @@ const Blog = ({ content, data }) => {
       <Header />
       <main className={post.main}>
         <h2>{frontmatter.title}</h2>
-        <ReactMarkdown
-          renderers={{ image: imageRenderer }}
-          children={content}
-        />
+        <Markdown
+            children={content}
+            remarkPlugins={[
+              remarkGfm,
+              unwrapImages,
+              remarkSlug,
+              [remarkAutolinkHeadings, { behavior: "wrap" }],
+              directive,
+            ]}
+            rehypePlugins={[rehypeRaw]}
+            components={{
+              img: (e) => {
+                return (
+                  <div
+                    style={{ position: "relative", width: "100%", height: 230 }}
+                  >
+                    <Image
+                      src={e.src}
+                      alt={e.alt}
+                      decoding={"async"}
+                      loading={"lazy"}
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </div>
+                );
+              },
+              link: (e) => {
+                if (e.href.match("http")) {
+                  return (
+                    <a href={e.href} target="_blank" rel="noopener noreferrer">
+                      {children}
+                    </a>
+                  );
+                }
+                if (e.href.slice(0, 1) == "#") {
+                  return <a href={e.href}>{e.children}</a>;
+                }
+                return (
+                  <Link href={e.href}>
+                    <a>{e.child}</a>
+                  </Link>
+                );
+              },
+            }} //rendererからcomponentsに変わった
+          />
       </main>
       <Footer />
     </>
